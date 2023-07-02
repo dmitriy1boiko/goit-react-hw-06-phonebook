33594cf8feb3cda990f8f77800bc6a3817848c33
Dmitriy Boiko <dmitriy1boiko@gmail.com>
2023-07-02 11:35:36 +0300

    rezult

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
+import Section from './section/Section';
+import Bookcontact from './bookcontact/Bookcontact';
+import { Container } from './App.styled';
+import { Contacts } from './contacts/Contacts';
+import { useState } from 'react';
+import { FilterContacts } from './filter-contacts/FilterContacts';
+
 export const App = () => {
+  // const [contacts, setContakts] = useState(
+  //   () => JSON.parse(localStorage.getItem('contacts')) ?? []
+  // );
+  const [filter, setFilter] = useState('');
+
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
+
+  // const findContact = contact => {
+  //   return contacts.find(
+  //     item => item.name.toLowerCase() === contact.name.toLowerCase()
+  //   );
+  // };
+
+  
+
+  //   setContakts(prev => [...prev, contact]);
+  // };
+
+  const handleChengeInput = filter => {
+    setFilter(filter);
+  };
+
+  // const applyFilters = () => {
+  //   return contacts.filter(
+  //     contact =>
+  //       contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+  //       contact.number.includes(filter)
+  //   );
+  // };
+
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section>
+        <Bookcontact></Bookcontact>
+      </Section>
+      <Section title="Contacts">
+        <FilterContacts filter={filter} onChangeInput={handleChengeInput} />
+        <Contacts
+
+        // onDeleteContact={handleDeleteContact}
+        />
+      </Section>
+    </Container>
   );
 };
---
+++ src/components/bookcontact/Bookcontact.jsx
@@ -0,0 +1,47 @@
+import { addContact } from 'redux/contactsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { nanoid } from 'nanoid';
+
+export default function Bookcontact() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const id = nanoid();
+    const name = e.target.elements.name.value;
+    const number = e.target.elements.number.value;
+    const item = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (item) {
+      alert(`${name} is alredy in contacts`);
+    } else {
+      dispatch(addContact({ id, name, number }));
+    }
+    e.target.reset();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <h3>Name</h3>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <h3>Number</h3>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+        <button type="submit">Add contact</button>
+      </div>
+    </form>
+  );
+}
