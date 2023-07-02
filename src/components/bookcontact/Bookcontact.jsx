1d1e9fb3689974688c1ba3b3615fe6ab2e45559d
Dmitriy Boiko <dmitriy1boiko@gmail.com>
2023-07-02 17:51:35 +0300

    forma_update

--- src/components/bookcontact/Bookcontact.jsx
+++ src/components/bookcontact/Bookcontact.jsx
@@ -1,10 +1,27 @@
 import { addContact } from 'redux/contactsSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 export default function Bookcontact() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -19,7 +36,8 @@ export default function Bookcontact() {
     } else {
       dispatch(addContact({ id, name, number }));
     }
-    e.target.reset();
+    setName('');
+    setNumber('');
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -31,6 +49,8 @@ export default function Bookcontact() {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          value={name}
+          onChange={handleChange}
         />
         <h3>Number</h3>
         <input
@@ -39,6 +59,8 @@ export default function Bookcontact() {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
+          value={number}
+          onChange={handleChange}
         />
         <button type="submit">Add contact</button>
       </div>
